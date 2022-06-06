c6141efa933495d5b41fb410966b8a956c3c1c10
Luis <luish77302@gmail.com>
2022-06-05 22:54:59 -0600

    Se agregaron botones para poder jugar al piedra, papel o tijera

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+const btn4 = document.querySelector("#piedra");
+btn4.addEventListener('click', () =>{
+    let computerAnswer = computerPlay();
+    playRound("Piedra",computerAnswer);
+});
+
+const btn3 = document.querySelector("#papel");
+btn3.addEventListener('click', () =>{
+    let computerAnswer = computerPlay();
+    playRound("Papel",computerAnswer);
+});
+
+const btn2 = document.querySelector("#tijera");
+btn2.addEventListener('click', () =>{
+    let computerAnswer = computerPlay();
+    playRound("Tijera",computerAnswer);
+});
+
+
 const computerPlay=()=>{
     let randomSelection  = Math.round(Math.random() * (3 - 1) + 1);
     switch(randomSelection){
@@ -20,42 +39,56 @@ const playerPlay=()=>{
     seleccion = seleccion.charAt(0).toUpperCase()+restOfWord;
     return seleccion;
 }
+
+const container = document.querySelector('.container');
+const results = document.createElement('div');
+
+const playerscore = document.createElement('p');
+const computerscore = document.createElement('p');
+playerscore.textContent = 0;
+playerscore.classList.add("playerscore");
+computerscore.classList.add("computerscore");
+computerscore.textContent = 0;
+results.append(playerscore);
+results.append(computerscore);
+container.append(results);
+
+let computerscoreselector= document.querySelector(".computerscore");
+let playerscoreselector = document.querySelector(".playerscore");
+
+let cs = computerscore.innerHTML;
+let ps = playerscore.innerHTML;
+
 const playRound = (playerSelection,computerSelection) => {
+
     if(playerSelection=="Piedra"){
         if(computerSelection=="Piedra"){
-            document.write("Empate");
+            alert("Empate");
         }else if(computerSelection=="Papel"){
-            document.write("Perdiste");
+            alert("Perdiste");
+            computerscoreselector.innerHTML = cs++;
         }else{
-            document.write("Ganaste");
+            alert("Ganaste");
+            playerscoreselector.innerHTML = ps++;
         }
         }
     if(playerSelection=="Papel"){
         if(computerSelection=="Piedra"){
-            document.write("Ganaste");
+            alert("Ganaste");
         }else if(computerSelection=="Tijera"){
-            document.write("Perdiste");
+            alert("Perdiste");
         }else{
-            document.write("Empate");
+            alert("Empate");
         }
         }
     if(playerSelection=="Tijera"){
         if(computerSelection=="Piedra"){
-            document.write("Perdiste");
+            alert("Perdiste");
         }else if(computerSelection=="Tijera"){
-            document.write("Empate");
+            alert("Empate");
         }else{
-            document.write("Ganaste");
+            alert("Ganaste");
         }
         }
     }                                                                                                                                                                                                                                                                                                                                                                                                                        
     
-const game= () =>{
-    for(let i=0;i<5;i++){
-        let computerAnswer = computerPlay();
-        let playerAnswer = playerPlay();
-        playRound(playerAnswer,computerAnswer);
-}
-}
-
-game();
